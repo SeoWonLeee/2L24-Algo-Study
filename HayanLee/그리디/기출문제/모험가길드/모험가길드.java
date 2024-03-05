c2eae73d487470c6a98a9a1e2de89e7d515f56d8
hayannn <102213509+hayannn@users.noreply.github.com>
2024-03-05 11:41:33 +0900

    :art: 그리디 기출 모험가길드 재풀이(정답 O)

--- HayanLee/그리디/기출문제/모험가길드/모험가길드.java
+++ HayanLee/그리디/기출문제/모험가길드/모험가길드.java
@@ -21,6 +21,7 @@ public class 모험가길드 {
             count += 1;
             if(count >= arr[i]){ // 현재 그룹 안 모험가수 >= 현재의 공포도 라면
                 result += 1; //그룹만들기
+                count = 0; //틀린 이유 -> 초기화를 진행하지 않아 '5'가 나옴 -> 그룹 안 모험가 수는 초기화히기(그래야 계속 루프를 돌며 그룹에 넣기 가능)
             }
         }
         System.out.println(result);
