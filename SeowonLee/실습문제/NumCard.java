dd1292f5947e99c22f77734e3c0b26a8daa8b327
dltjdnjs <148112372+SeoWonLeee@users.noreply.github.com>
2024-03-05 16:36:18 +0900

    fix: 입출력 조건 고려한 코드 수정

--- SeowonLee/실습문제/NumCard.java
+++ SeowonLee/실습문제/NumCard.java
@@ -1,63 +1,35 @@
 package Greedy;
 
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class NumCard {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 숫자 카드 배열 입력
-        System.out.println("배열 입력");
-        String input = scanner.nextLine();
-        String[] parts = input.split(" / "); // 행과 열로 분리
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
-        // 행과 열의 개수 파악
-        String[] row1 = parts[0].split(" ");
-        int n = Integer.parseInt(row1[0]); // 행의 개수
-        int m = Integer.parseInt(row1[1]); // 열의 개수
+        int[][] array= new int[N][M];
+        int[] minInRow = new int[N]; // 각 행의 가장 작은 수의 배열
 
-        // 배열 생성
-        int[][] cards = new int[n][m];
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                array[i][j] = scanner.nextInt();
+            }
 
-        // 입력된 배열 저장
-        for (int i = 0; i < n; i++) {
-            String[] nums = parts[i + 1].split(" ");
-            int len = Math.min(nums.length, m); // 열의 개수와 입력된 행의 길이 중 작은 값 선택
-            for (int j = 0; j < len; j++) {
-                cards[i][j] = Integer.parseInt(nums[j]);
+            Arrays.sort(array[i]);
+            minInRow[i] = array[i][0]; // 각 행의 가장 작은 수
+        }
+
+        // 최솟값 중 가장 큰 값
+        int maxOfMin = minInRow[0];
+        for(int i=1; i<N; i++) {
+            if(minInRow[i] > maxOfMin) {
+                maxOfMin = minInRow[i];
             }
         }
 
-        // 입력 받은 배열 출력
-        System.out.println("배열 :");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(cards[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        // 각 행의 최솟값을 저장할 배열 생성
-        int[] minInRows = new int[n];
-
-        // 각 행의 최솟값 계산하여 배열에 저장
-        for (int i = 0; i < n; i++) {
-            Arrays.sort(cards[i]); // 행 정렬
-            minInRows[i] = cards[i][0]; // 최솟값 저장
-        }
-
-        // 최솟값 중 가장 큰 값 찾기
-        int maxOfMin = minInRows[0];
-        for (int i = 1; i < n; i++) {
-            if (minInRows[i] > maxOfMin) {
-                maxOfMin = minInRows[i];
-            }
-        }
-
-        // 결과 출력
-        System.out.println("결과 : " + maxOfMin);
-
-        scanner.close();
+        System.out.println(maxOfMin);
     }
 }
