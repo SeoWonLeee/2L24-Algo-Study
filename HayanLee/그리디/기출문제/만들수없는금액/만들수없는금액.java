6a2c434141ebbcfd26fa8ffac55350446ef4e3e7
hayannn <102213509+hayannn@users.noreply.github.com>
2024-03-06 16:32:19 +0900

    :art: 그리디 기출 만들수없는금액 재풀이(정답O)

--- HayanLee/그리디/기출문제/만들수없는금액/만들수없는금액.java
+++ HayanLee/그리디/기출문제/만들수없는금액/만들수없는금액.java
@@ -1,7 +1,7 @@
 package HayanLee.그리디.기출문제.만들수없는금액;
 
 import java.util.*;
-//오후 3시 20분 시작
+//오후 3시 20분 시작 ~ 4시 25분 종료
 public class 만들수없는금액 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -16,22 +16,11 @@ public class 만들수없는금액 {
         //2. 배열 정렬하기
         Arrays.sort(arr);
 
-        //3. 없는 수를 저장할 배열 만들어주기
-        int[] none = new int[N];
+        //3. 만들 수 없는 금액을 찾은 경우 종료하기
 
-        int result = 0;
-
-        //4. 1 ~ N까지의 수 중에서 배열에 없는 수 골라내기
-        for(int i=0; i<arr.length; i++) {
-            if (arr[i] <= none[i]) {
-                none[i] += arr[i];
-            }
-        }
-
-        //5. 만약, 배열에 있는 숫자를 조합해 더한 값 != 없는 숫자라면 그게 result
-        for(int i=0; i<N; i++){
-            int sum = Arrays.stream(arr).sum();
-            if(sum != none[i]){
+        int result = 1;
+        for(int i=0; i<N; i++) {
+            if (result < arr[i]) {
                 break;
             }
             result += arr[i];
