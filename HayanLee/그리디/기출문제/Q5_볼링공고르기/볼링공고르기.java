edb3ffadf814732476407ba3d51cbf2cf99511af
hayannn <102213509+hayannn@users.noreply.github.com>
2024-03-06 17:35:18 +0900

    :art: 그리디 기출 볼링공고르기 재풀이(정답 O)

--- HayanLee/그리디/기출문제/Q5_볼링공고르기/볼링공고르기.java
+++ HayanLee/그리디/기출문제/Q5_볼링공고르기/볼링공고르기.java
@@ -1,13 +1,12 @@
 package HayanLee.그리디.기출문제.Q5_볼링공고르기;
 
-// 오후 4시 45분 시작
+//오후 4시 45분 시작 ~ 5시 13분 종료
 import java.util.*;
-
 public class 볼링공고르기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 1. N, M, 배열 입력받기
+        //1. N, M, 배열 입력받기
         int N = sc.nextInt();
         int M = sc.nextInt();
 
@@ -16,30 +15,21 @@ public class 볼링공고르기 {
             arr[i] = sc.nextInt();
         }
 
-        int r = 2; // 볼링공 2개를 고르는 경우이므로 r은 2로 설정
-        boolean[] visited = new boolean[N];
-
-        // 3. nCr 조합으로 계산하기(백트래킹 사용)
-        int result = combination(arr, visited, 0, N, r);
+        int result = combination(arr, N, 2);
         System.out.println(result);
     }
 
-    // 2. nCr 조합으로 계산하기(백트래킹 사용)
-    static int combination(int[] arr, boolean[] visited, int start, int N, int r) {
-        if (r == 0) {
-            return 1; // 조합을 찾았을 때마다 1을 반환하여 개수를 세어줍니다.
-        }
-
+    //2. nCr 조합으로 계산하기(백트래킹 사용)
+    static int combination(int[] arr, int N, int r) {
         int count = 0;
 
-        for (int i = start; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                count += combination(arr, visited, i + 1, N, r - 1);
-                visited[i] = false;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] != arr[j]) { //문제의 조건에 두 사람은 서로 다른 무게를 골라야한다고 했기 때문.
+                    count++;
+                }
             }
         }
-
         return count;
     }
 }
