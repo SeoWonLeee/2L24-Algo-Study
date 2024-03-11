1d0a06747577b57fd9ea763b211c580a060ccc58
hayannn <dlgkdis801@naver.com>
2024-03-11 23:27:42 +0900

    chore: 구현 기출 외벽점검 재풀이(정답) - 레퍼런스 참고

--- HayanLee/구현/기출문제/Q14_외벽점검/외벽점검.java
+++ HayanLee/구현/기출문제/Q14_외벽점검/외벽점검.java
@@ -1,6 +1,5 @@
 package HayanLee.구현.기출문제.Q14_외벽점검;
 
-//오후 10시 15분 ~ 10시 51분
 /*
 알고리즘
 - 외벽의 길이 n의 범위 내에서
@@ -11,91 +10,69 @@ package HayanLee.구현.기출문제.Q14_외벽점검;
 - 별도 메서드 : 현재 시작 지점에서 외벽을 점검가능 여부 체크, 점검한 취약 지점 개수 세기
 */
 
-//오후 10시 15분 ~
-import java.util.ArrayList;
-import java.util.Arrays;
+/*
+알고리즘2
+출처 : https://sc-science.tistory.com/93
+원형 배열의 시작점을 하나씩 바꿔가면서, 친구dist의 순열을 구하여
+각 경우마다 최소 필요한 친구 수를 구하기
 
-public class Solution {
-    static int n;
-    static ArrayList<Integer> weakList;
-    static int[] dist;
-    static boolean[] visited;
+어떤 경우에도 취약점을 모두 점검할 수 없다면 return -1
+모든 취약점을 점검하는데 필요한 최소 친구 수를 구하기
+*/
 
-    public int solution(int N, int[] weak, int[] dist) {
-        n = N;
-        weakList = new ArrayList<>();
-        for (int w : weak) {
-            weakList.add(w);
-        }
-        this.dist = dist;
-        visited = new boolean[dist.length];
+//오후 10시 15분 ~ 10시 51분
+//오답 풀이 ~ 11시 20분
+import java.util.*;
 
-        int answer = Integer.MAX_VALUE;
+class Solution {
+    static public int[] weakList;
+    static public int answer;
+    public int solution(int n, int[] weak, int[] dist) {
 
-        // 이동 거리 내림차순 정렬
-        Arrays.sort(dist);
-
-        for (int i = 0; i < weak.length; i++) {
-            // 취약 지점을 원형으로 생각하여 시작 지점 변경
-            int[] circularWeak = new int[weak.length * 2];
-            for (int j = 0; j < weak.length; j++) {
-                circularWeak[j] = weak[j] + n;
-                circularWeak[j + weak.length] = weak[j];
-            }
-
-            // 외벽을 순회하며 각 시작점에 대해 점검 가능한지 확인
-            for (int j = i; j < i + weak.length; j++) {
-                int start = circularWeak[j];
-
-                // 외벽 점검 진행
-                if (check(start)) {
-                    answer = Math.min(answer, countVisited());
-                }
-            }
+        answer = Integer.MAX_VALUE;
+        weakList = new int[weak.length*2];
+        int i=0;
+        while (i<weak.length){
+            weakList[i] = weak[i];
+            weakList[i + weak.length] = weak[i++]+n;
         }
 
-        // 점검이 불가능한 경우 -1 반환
-        return (answer == Integer.MAX_VALUE) ? -1 : answer;
+        for(int k=0; k<weak.length; k++) {// 시작점 옮기기
+            dfs(k, 0, dist, new boolean[dist.length], new int[dist.length]);
+        }
+
+        if(answer==Integer.MAX_VALUE) return -1;
+
+        return answer;
     }
 
-    // 현재 시작 지점에서 외벽을 점검할 수 있는지 확인하는 메소드
-    private boolean check(int start) {
-        for (int i = 0; i < dist.length; i++) {
-            int curDist = dist[i];
+    public void dfs(int start, int depth, int[] dist, boolean[] visited, int[] permuted) {
+        //permuted -> dist의 순열을 나타냄.
+        if(depth==dist.length) {
+            answer = Math.min(answer, check(start, start+weakList.length/2, permuted));
+            return;
+        }
+        for(int i=0; i<dist.length; i++) {
+            if(visited[i]) continue;
 
-            // 이동 가능한 거리만큼 반복하며 점검 가능한지 확인
-            for (int j = start; j < start + curDist; j++) {
-                if (!weakList.contains(j % n)) {
-                    weakList.remove(Integer.valueOf(j % n));
-                }
-            }
             visited[i] = true;
-
-            // 모든 취약 지점을 점검할 수 있는 경우 true 반환
-            if (weakList.isEmpty()) {
-                return true;
-            }
-
-            // 취약 지점을 초기화하고 다음 이동 거리에 대해 점검 진행
-            weakList.clear();
-            for (int w : weakList) {
-                weakList.add(w);
-            }
+            permuted[depth] = dist[i];
+            dfs(start, depth+1, dist, visited, permuted);
             visited[i] = false;
         }
-
-        // 모든 이동 거리에 대해 점검이 불가능한 경우 false 반환
-        return false;
     }
 
-    // 점검한 취약 지점 개수를 세는 메소드
-    private int countVisited() {
-        int count = 0;
-        for (boolean v : visited) {
-            if (v) {
-                count++;
+    public int check(int start, int end, int[] permuted) {
+        int friend = 1;// permuted의 index
+        int pos = weakList[start]+permuted[friend-1];// 시작점 : 첫 취약점의 위치+친구거리
+
+        for(int i=start; i<end; i++) {
+            if(pos < weakList[i]) {// 점검 위치 벗어나면
+                friend++; // 친구추가
+                if (friend > permuted.length) return Integer.MAX_VALUE;
+                pos = weakList[i] + permuted[friend-1];
             }
         }
-        return count;
+        return friend;
     }
 }
