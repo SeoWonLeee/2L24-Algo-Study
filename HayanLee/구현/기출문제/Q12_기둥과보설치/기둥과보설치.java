d88cc5f05f400f67ce0396c708c3ec5a59595c51
hayannn <dlgkdis801@naver.com>
2024-03-11 21:00:41 +0900

    chore: 구현 기출 기둥과 보 설치 재풀이(정답)

--- HayanLee/구현/기출문제/Q12_기둥과보설치/기둥과보설치.java
+++ HayanLee/구현/기출문제/Q12_기둥과보설치/기둥과보설치.java
@@ -1,87 +1,92 @@
 package HayanLee.구현.기출문제.Q12_기둥과보설치;
 
-//오후 7시 45분 ~
+//오후 7시 45분 ~ 8시 27분
+//풀이 이해가 더 필요함..
+
 import java.util.*;
-
-/*
-- 입력받은 배열에서 [x,y,a,b] 형식으로 기둥과 보 설치or삭제하기
-- 기둥을 설치
-- 기둥을 삭제
-- 보를 설치
-- 보를 삭제
-- 벽면을 벗어나지 않는다 + 바닥에 보를 설치하지 않는다.즉, (n,0)의 좌표에서는 보 설치 금지(시작점인 (1,0)은 제외)
-*/
-
 
 class Solution {
     public int[][] solution(int n, int[][] build_frame) {
+        List<int[]> result = new ArrayList<>();  // 최종 결과를 저장할 리스트
 
-        //1. 기둥과 보 정보 선언
-        boolean[][] leng = new boolean[n+1][n+1]; //기둥
-        boolean[][] wid = new boolean[n+1][n+1]; //보
+        boolean[][] leng = new boolean[n + 1][n + 1];  // 기둥 정보
+        boolean[][] wid = new boolean[n + 1][n + 1];    // 보 정보
 
-        int count = 0; //설치할 때마다 추가하고, 삭제할 때마다 빼주기
+        for (int[] frame : build_frame) {
+            int x = frame[0];
+            int y = frame[1];
+            int a = frame[2];
+            int b = frame[3];
 
-        //2. [x,y,a,b] 형식 선언
-        for(int i=0; i<build_frame.length; i++){
-            int x = build_frame[i][0]; //점의 x좌표
-            int y = build_frame[i][1]; //점의 y좌표
-            int a = build_frame[i][2]; //a = 설치 또는 삭제할 구조물(0=기둥, 1=보)
-            int b = build_frame[i][3]; //b = 구조물을 설치 or 삭제 결정(0=삭제, 1=삭제)
-        }
-
-        //3. 기둥 설치
-        if(a == 0){
-            if(b == 1){
-                leng[x][y] = true;
-                count++;
-            }
-        }
-        //4. 기둥 삭제
-        if(a == 0){
-            if(b == 0)
-                leng[x][y] = false;
-            count--;
-        }
-
-        //5. 보 설치
-        if(a == 1){
-            if(b == 1)
-                wid[x][y] = true;
-            count--;
-        }
-
-        //6. 보 삭제
-        if(a == 1){
-            if(b == 0)
-                wid[x][y] = false;
-            count--;
-        }
-
-
-        //7. 벽면을 벗어나지 않는다 + 바닥에 보를 설치하지 않는다.즉, (n,0)의 좌표에서는 보 설치 금지(시작점인 (1,0)은 제외)
-        if(y == 0){
-            return true;
-        }
-
-        //8. 결과 리턴
-        int[][] answer = new int[count][3];
-        int idx = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(leng[i][j]){
-                    answer[idx][0] = i;
-                    answer[idx][1] = j;
-                    answer[idx++][2] = 0;
+            if (b == 1) {  // 설치하는 경우
+                if (a == 0) {  // 기둥 설치
+                    if (y == 0 || leng[x][y - 1] || (x > 0 && wid[x - 1][y]) || wid[x][y]) {
+                        leng[x][y] = true;
+                    }
+                } else {  // 보 설치
+                    if ((leng[x][y - 1] || leng[x + 1][y - 1]) || ((x > 0 && wid[x - 1][y]) && (x < n && wid[x + 1][y]))) {
+                        wid[x][y] = true;
+                    }
                 }
-                if(wid[i][j]){
-                    answer[idx][0] = i;
-                    answer[idx][1] = j;
-                    answer[idx++][2] = 1;
+            } else {  // 삭제하는 경우
+                if (a == 0) {  // 기둥 삭제
+                    leng[x][y] = false;
+                    boolean valid = true;
+
+                    // 기둥 삭제 후 유효성 검사
+                    if ((y < n && leng[x][y + 1] && !isValidLeng(x, y + 1, leng, wid)) ||
+                            (x > 0 && wid[x - 1][y + 1] && !isValidWid(x - 1, y + 1, leng, wid)) ||
+                            (wid[x][y + 1] && !isValidWid(x, y + 1, leng, wid))) {
+                        valid = false;
+                        leng[x][y] = true;  // 삭제 전 상태로 복구
+                    }
+
+                    if (!valid) continue;  // 유효하지 않으면 삭제 취소
+                } else {  // 보 삭제
+                    wid[x][y] = false;
+                    boolean valid = true;
+
+                    // 보 삭제 후 유효성 검사
+                    if (((leng[x][y] && !isValidLeng(x, y, leng, wid)) ||
+                            (x < n && leng[x + 1][y] && !isValidLeng(x + 1, y, leng, wid))) ||
+                            ((x > 0 && wid[x - 1][y] && !isValidWid(x - 1, y, leng, wid)) ||
+                                    (x < n && wid[x + 1][y] && !isValidWid(x + 1, y, leng, wid)))) {
+                        valid = false;
+                        wid[x][y] = true;  // 삭제 전 상태로 복구
+                    }
+
+                    if (!valid) continue;  // 유효하지 않으면 삭제 취소
                 }
             }
-
         }
-        return answer;
+
+        // 최종 결과를 리스트에 저장
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (leng[i][j]) result.add(new int[]{i, j, 0});
+                if (wid[i][j]) result.add(new int[]{i, j, 1});
+            }
+        }
+
+        // 정렬 후 배열로 변환하여 반환
+        Collections.sort(result, (a, b) -> {
+            if (a[0] == b[0]) {
+                if (a[1] == b[1]) {
+                    return Integer.compare(a[2], b[2]);
+                }
+                return Integer.compare(a[1], b[1]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+
+        return result.toArray(new int[0][]);
+    }
+
+    private boolean isValidLeng(int x, int y, boolean[][] leng, boolean[][] wid) {
+        return y == 0 || leng[x][y - 1] || (x > 0 && wid[x - 1][y]) || wid[x][y];
+    }
+
+    private boolean isValidWid(int x, int y, boolean[][] leng, boolean[][] wid) {
+        return (leng[x][y - 1] || leng[x + 1][y - 1]) || ((x > 0 && wid[x - 1][y]) && (x < wid.length - 1 && wid[x + 1][y]));
     }
 }
