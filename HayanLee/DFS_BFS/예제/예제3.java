57c5e80d1cb26a75fdc077c7ddec22e7fa149353
hayannn <dlgkdis801@naver.com>
2024-03-12 14:39:21 +0900

    :sparkles: DFS/BFS 이론 예제3 코드

---
+++ HayanLee/DFS_BFS/예제/예제3.java
@@ -0,0 +1,16 @@
+package HayanLee.DFS_BFS.예제;
+
+import java.util.*;
+
+public class 예제3 {
+
+    public static void recursiveFunction() {
+        System.out.println("재귀 함수를 호출합니다.");
+        recursiveFunction();
+    }
+
+    public static void main(String[] args) {
+        recursiveFunction();
+    }
+
+}
