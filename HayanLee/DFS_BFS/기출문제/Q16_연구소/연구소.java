560397309b579f8b3aa438c4c4930569b725fc30
hayannn <dlgkdis801@naver.com>
2024-03-14 16:51:51 +0900

    chore: DFS/BFS 기출 연구소 재풀이(정답)

--- HayanLee/DFS_BFS/기출문제/Q16_연구소/연구소.java
+++ HayanLee/DFS_BFS/기출문제/Q16_연구소/연구소.java
@@ -1,98 +1,102 @@
 package HayanLee.DFS_BFS.기출문제.Q16_연구소;
 
-//오후 3시 53분 ~
+//오후 3시 53분 ~ 4시 45분
 
 /*
 알고리즘
-0. 전역 변수 선언(N, M, dx, dy, result)
+0. 전역 변수 선언(N, M, arr, dx, dy, result)
 1. N과 M 입력받기
-2. 버퍼 비우기
-3. 배열 입력받기(String)
+2. 배열 입력받기
 4. DFS 실행
    - 안전 영역 수를 세는 메서드
    - 조합으로 벽의 위치를 뽑는 메서드
  */
 import java.util.*;
+
 public class 연구소 {
-
-    static int N;
-    static int M;
+    static int N, M;
     static int[][] arr;
-    static int[] dx = {0, -1, 0, 1};
-    static int[] dy = {-1, 0, 1, 0};
-
     static int result = 0;
-    static int count = 0;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         M = sc.nextInt();
 
         arr = new int[N][M];
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        combination(0,0);
+        combination(0);
         System.out.println(result);
     }
 
-    //DFS
-    public static void dfs(int[][] vi, int x, int y){
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                if(vi[i][j] == 2) dfs(vi, i, j);
-            }
-        }
-
-        for(int i=0; i<N; i++){
+    // DFS
+    public static void dfs(int[][] vi, int x, int y) {
+        for(int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx >= 0 && ny >=0 && nx < N && ny < M){
-                if(vi[nx][ny] == 0){
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                if(vi[nx][ny] == 0) {
                     vi[nx][ny] = 2;
                     dfs(vi, nx, ny);
                 }
             }
         }
+    }
 
-        int count = 0;
+    // 안전 영역 크기 구하기
+    public static int getSafeArea(int[][] vi) {
+        int safeArea = 0;
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < M; j++) {
-                if(vi[i][j] == 0){
-                    count++;
+                if(vi[i][j] == 0) {
+                    safeArea++;
                 }
             }
         }
-        //return count;
+        return safeArea;
     }
 
-    public static void combination(int x, int y) {
-        if(x == 3) {
+    // 벽을 세우는 모든 경우의 수 탐색
+    public static void combination(int count) {
+        if(count == 3) { // 벽을 3개 세운 경우
             int[][] vi = new int[N][M];
             for(int i = 0; i < N; i++) {
                 for(int j = 0; j < M; j++) {
-                    vi[i][j] = vi[i][j];
+                    vi[i][j] = arr[i][j];
                 }
             }
-            result = Math.max(result, count);
+
+            // 바이러스 퍼뜨리기
+            for(int i = 0; i < N; i++) {
+                for(int j = 0; j < M; j++) {
+                    if(vi[i][j] == 2) {
+                        dfs(vi, i, j);
+                    }
+                }
+            }
+
+            // 안전 영역 크기 구하기
+            result = Math.max(result, getSafeArea(vi));
             return;
         }
 
-        for(int i = x; i < N * M; i++) {
-            int nx = i / M;
-            int ny = i % M;
-            if(arr[nx][ny] == 0) {
-                arr[nx][ny] = 1;
-                combination(x + 1, i + 1);
-                arr[nx][ny] = 0;
+        // 조합 탐색
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(arr[i][j] == 0) {
+                    arr[i][j] = 1;
+                    combination(count + 1);
+                    arr[i][j] = 0;
+                }
             }
         }
     }
-
-
 }
