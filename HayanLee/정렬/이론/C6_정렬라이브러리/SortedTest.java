27fb3f8fab446b72bf86292c139f774bd4e1e5eb
hayannn <dlgkdis801@naver.com>
2024-03-17 16:55:52 +0900

    :sparkles: SortedTest.java 생성

---
+++ HayanLee/정렬/이론/C6_정렬라이브러리/SortedTest.java
@@ -0,0 +1,17 @@
+package HayanLee.정렬.이론.C6_정렬라이브러리;
+
+import java.util.*;
+
+public class SortedTest {
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        int[] sortedArray = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(sortedArray);
+
+        String result = Arrays.toString(sortedArray);
+        System.out.println(result);
+    }
+}
