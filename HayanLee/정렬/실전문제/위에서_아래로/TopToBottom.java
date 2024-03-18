1166ccf93c4b248e1fc7c0055ce05c425723230c
hayannn <102213509+hayannn@users.noreply.github.com>
2024-03-18 17:47:49 +0900

    chore: TopToBottom 재풀이(정답)

--- HayanLee/정렬/실전문제/위에서_아래로/TopToBottom.java
+++ HayanLee/정렬/실전문제/위에서_아래로/TopToBottom.java
@@ -10,7 +10,8 @@ public class TopToBottom {
 
         int N = sc.nextInt();
 
-        int[] arr = new int[N];
+        //int[] arr = new int[N];
+        Integer[] arr = new Integer[N];
         for(int i=0; i<N; i++){
             arr[i] = sc.nextInt();
         }
