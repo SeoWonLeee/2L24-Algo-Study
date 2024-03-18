a22dcc7b77bc7904478e6236cf59b3f27078e72e
hayannn <102213509+hayannn@users.noreply.github.com>
2024-03-18 17:46:13 +0900

    chore: TopToBottom 첫풀이(오답)

--- HayanLee/정렬/실전문제/위에서_아래로/TopToBottom.java
+++ HayanLee/정렬/실전문제/위에서_아래로/TopToBottom.java
@@ -1,4 +1,27 @@
 package HayanLee.정렬.실전문제.위에서_아래로;
 
+//오후 5시 36분 ~ 5시 45분
+
+import java.util.*;
+
 public class TopToBottom {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int i=0; i<N; i++) {
+            System.out.println(arr[i] + " ");
+        }
+
+
+    }
 }
