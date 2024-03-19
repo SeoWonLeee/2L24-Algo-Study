ad2cdcb92703cd66705fb4af734098c1fee7f255
hayan <102213509+hayannn@users.noreply.github.com>
2024-03-19 15:19:04 +0900

    chore: CardSort 재풀이(정답)

--- HayanLee/정렬/기출문제/Q26_카드정렬하기/CardSort.java
+++ HayanLee/정렬/기출문제/Q26_카드정렬하기/CardSort.java
@@ -1,30 +1,43 @@
 package HayanLee.정렬.기출문제.Q26_카드정렬하기;
 // 14:34 ~ 14: 42
+//재풀이 ~ 3시 10분
 /*
 [알고리즘]
 1. N 입력받기
 2. 배열 입력받기
-3. 배열을 내림차순 정렬하기
-4. 배열의 합 구하기
+3. 배열을 내림차순 정렬하기 -> 오름차순으로 정렬해야 함!
+4. 카드 합치기
+    - 우선순위 큐를 이용해 매번 가장 작은 두 카드를 선택해 합치기
+5. 결과 출력
+
  */
 import java.util.*;
+
 public class CardSort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        Integer[] arr = new Integer[N];
-        for(int i=0; i<N; i++){
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr, Collections.reverseOrder());
+        //Arrays.sort(arr);
 
-        int sum = 0;
+        int result = 0;
+        Queue<Integer> pq = new PriorityQueue<>();
         for(int i=0; i<N; i++){
-            sum += arr[i];
+            pq.offer(arr[i]);
         }
 
-        System.out.print(sum);
+        while(pq.size() > 1){
+            int first = pq.poll();
+            int second = pq.poll();
+            int sum = first + second;
+            result += sum;
+            pq.offer(sum);
+        }
+        System.out.println(result);
     }
 }
