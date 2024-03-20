4a4e7b5d44302b18a10d447e2f390c5bc4c31fc3
hayan <102213509+hayannn@users.noreply.github.com>
2024-03-20 14:48:30 +0900

    :recycle: 이진탐색 이론, 이론예제코드 패키지 수정

--- HayanLee/이진탐색/이론/이론예제코드/BinarySearchEx1.java
+++ HayanLee/이진탐색/이론/이론예제코드/BinarySearchEx1.java
@@ -1,21 +1,19 @@
-package HayanLee.이진탐색.이론예제코드;
+package HayanLee.이진탐색.이론.이론예제코드;
 
 import java.util.*;
 
-public class BinarySearchEx2 {
+public class BinarySearchEx1 {
 
-    // 이진 탐색 소스코드 구현(반복문)
+    // 이진 탐색 소스코드 구현(재귀 함수)
     public static int binarySearch(int[] arr, int target, int start, int end) {
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            // 찾은 경우 중간점 인덱스 반환
-            if (arr[mid] == target) return mid;
-                // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
-            else if (arr[mid] > target) end = mid - 1;
-                // 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
-            else start = mid + 1;
-        }
-        return -1;
+        if (start > end) return -1;
+        int mid = (start + end) / 2;
+        // 찾은 경우 중간점 인덱스 반환
+        if (arr[mid] == target) return mid;
+            // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+        else if (arr[mid] > target) return binarySearch(arr, target, start, mid - 1);
+            // 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+        else return binarySearch(arr, target, mid + 1, end);
     }
 
     public static void main(String[] args) {
