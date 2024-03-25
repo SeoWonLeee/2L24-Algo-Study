4fc2c9f41d17a9f5838e90fe8374686cda0bce1f
hayannn <102213509+hayannn@users.noreply.github.com>
2024-03-25 09:30:12 +0900

    chore: FillFloor 첫풀이(오답)

--- HayanLee/다이나믹프로그래밍/실전문제/바닥공사/FillFloor.java
+++ HayanLee/다이나믹프로그래밍/실전문제/바닥공사/FillFloor.java
@@ -1,4 +1,28 @@
 package HayanLee.다이나믹프로그래밍.실전문제.바닥공사;
 
+// 오전 9시 15분 ~ 9시 25분
+
+/*
+[알고리즘]
+1. N 입력받기
+2. 다이나믹 프로그래밍 진행하기
+   - BottomUp방식으로
+ */
+
+import java.util.*;
+
 public class FillFloor {
+
+    static int[] d = new int[1001];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i=2; i<=N; i++){
+            d[i] = (d[i-1] + 2*d[i-2]);
+
+        }
+        System.out.println(d[N] % 796796);
+    }
 }
